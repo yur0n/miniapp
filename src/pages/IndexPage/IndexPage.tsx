a4e8fda28f00ee7fb7c7302ee15eb08a29a7c9fe
Yuri <yur0n@yahoo.com>
2024-10-01 21:48:38 +0300

    up

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -25,7 +25,7 @@ export const IndexPage: FC = () => {
         header='Application Launch Data'
         footer='These pages help developer to learn more about current launch information'
       >
-        <Link to='/init-data'>
+        <Link to='/init-data'> 
           <Cell subtitle='User data, chat information, technical data'>Init Data</Cell>
         </Link>
         <Link to='/launch-params'>
